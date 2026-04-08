d9deef1e802cf87cae20f5fb665b4147cf015a42
Aadarsh Kumar <151125358+RathourAadarsh@users.noreply.github.com>
2026-04-08 17:26:56 +0530

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <style>
+        .swal-footer {
+            text-align: center !important;
+        }
+
+        .swal-text {
+            font-size: 22px !important;
+            text-align: center;
+        }
+
+        .swal-button {
+            background-color: #184f34 !important;
+        }
+    </style>
+    <script type="text/javascript" src="sweetalertnew.js"></script>
+    <script type="text/javascript">
+        function savealert(message, type) {
+            swal({
+                text: message,
+                icon: type === 'success' ? 'success' : type === 'info' ? 'info' : type === 'error' ? 'error' : 'warning',
+            });
+        }
+    </script>
+    <script type="text/javascript">
+        window.history.forward();
+        function noBack() {
+            window.history.forward();
+        }
+    </script>
+    <title>Login Panel</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css" />
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css" />
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/util.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="limiter">
+            <div class="container-login100">
+                <div class="wrap-login100">
+                    <div class="login100-form validate-form">
+                        <span class="login100-form-title">Login Panel
+                        </span>
+                        <h6 runat="server" id="loginsms" style="color:darkgreen;font-weight:600;"></h6>
+                        <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                            <asp:TextBox ID="txtuserid" runat="server" CssClass="input100" placeholder="Username"></asp:TextBox>
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div class="wrap-input100 validate-input" data-validate="Password is required">
+                            <asp:TextBox ID="txtpassword" runat="server" CssClass="input100"
+                                TextMode="Password" placeholder="Password"></asp:TextBox>
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-lock" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div class="wrap-input100 validate-input" data-validate="Password is required">
+                            <asp:DropDownList ID="ddlfinyear" runat="server" CssClass="input100"
+                                placeholder="Password">
+                            </asp:DropDownList>
+                            <span class="focus-input100"></span>
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <asp:Button ID="btnLogin" runat="server" Text="Login" CssClass="login100-form-btn" OnClick="btnLogin_Click" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Login : System.Web.UI.Page
+{
+    public static TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+    public static DateTime dateTime;
+    Class1 cl = new Class1();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dateTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneInfo);
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.Cache.SetNoStore();
+        Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+        if (!IsPostBack)
+        {
+            HttpCookie logincookie = Request.Cookies["userauth"];
+            if (Request.Cookies["userauth"] != null)
+            {
+                HttpCookie userAuthCookie = new HttpCookie("userauth");
+                userAuthCookie.Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies.Add(userAuthCookie);
+            }
+            bindfinyear();
+        }
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (txtuserid.Text == "")
+            {
+                string message = "Please Enter Username !!";
+                string type = "warning";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "savealert('" + message + "','" + type + "')", true);
+                return;
+            }
+            else if (txtpassword.Text == "")
+            {
+                string message = "Please Enter Password !!";
+                string type = "warning";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "savealert('" + message + "','" + type + "')", true);
+                return;
+            }
+            else
+            {
+                string query = "select name,username,password,type from Login where username='" + txtuserid.Text + "' and password='" + txtpassword.Text + "' and isActive='0'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, cl.con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    loginsms.InnerText = "You have logged Successfully !!";
+                    string usertype = dt.Rows[0]["type"].ToString();
+                    HttpCookie logincookie = new HttpCookie("userauth");
+                    logincookie["name"] = dt.Rows[0]["name"].ToString();
+                    logincookie["username"] = txtuserid.Text;
+                    logincookie["finyear"] = ddlfinyear.SelectedValue;
+                    logincookie["type"] = usertype;
+                    logincookie.Expires = DateTime.Now.AddYears(1);
+                    if (usertype == "Admin")
+                    {
+                        Response.Cookies.Add(logincookie);
+                        Response.Redirect("Dashboard.aspx");
+                        return;
+                    }
+                }
+                else
+                {
+                    string query1 = "select ac_code,ac_name as name,mobile as username,ac_code,'User' as type from partyMaster where mobile='" + txtuserid.Text + "' and ac_code='" + txtpassword.Text + "'";
+                    SqlDataAdapter sda1 = new SqlDataAdapter(query1, cl.con);
+                    DataTable dt1 = new DataTable();
+                    sda1.Fill(dt1);
+                    if (dt1.Rows.Count > 0)
+                    {
+                        string usertype = dt1.Rows[0]["type"].ToString();
+                        HttpCookie logincookie = new HttpCookie("userauth");
+                        logincookie["name"] = dt1.Rows[0]["name"].ToString();
+                        logincookie["ac_code"] = dt1.Rows[0]["ac_code"].ToString();
+                        logincookie["username"] = txtuserid.Text;
+                        logincookie["finyear"] = ddlfinyear.SelectedValue;
+                        logincookie["type"] = usertype;
+                        logincookie.Expires = DateTime.Now.AddYears(1);
+                            Response.Cookies.Add(logincookie);
+                            Response.Redirect("User/Dashboard.aspx");
+                            return;
+                    }
+                    else
+                    {
+                        string message = "Invalid Login Credentials !!";
+                        string type = "error";
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "savealert('" + message + "','" + type + "')", true);
+                        return;
+                    }
+                }
+               
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+    public void bindfinyear()
+    {
+        try
+        {
+            string query = @"DECLARE @today DATE = GETDATE();
+            DECLARE @year INT;
+            IF MONTH(@today) >= 4
+                SET @year = YEAR(@today);
+            ELSE
+                SET @year = YEAR(@today) - 1;
+            SELECT CAST(@year AS VARCHAR) +'-' + RIGHT(CAST(@year + 1 AS VARCHAR), 2) AS FinancialYear";
+
+            SqlDataAdapter da = new SqlDataAdapter(query, cl.con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                ddlfinyear.DataSource = dt;
+                ddlfinyear.DataTextField = "FinancialYear";
+                ddlfinyear.DataValueField = "FinancialYear";
+                ddlfinyear.DataBind();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
